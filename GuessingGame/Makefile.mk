2772d1c4d0438af74202d1f996ae13e1226aaa07
Youssef Alouani <133816408+Al0uani@users.noreply.github.com>
2024-03-12 23:13:23 +0000

    Delete GuessingGame/Makefile.mk

--- GuessingGame/Makefile.mk
+++
@@ -1,8 +0,0 @@
-README.md: guessinggame.sh
-    echo "# Guessing Game" > README.md
-    echo "Make was run on: $$(date)" >> README.md
-    echo "Number of lines in guessinggame.sh: $$(wc -l < guessinggame.sh)" >> README.md
-
-.PHONY: clean
-clean:
-    rm README.md
